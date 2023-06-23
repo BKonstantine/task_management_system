471970c61494d10309b5ee0dc7022dddebd3a680
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-23 14:11:21 +0300

    feat: Добавить компонент SvgIcon

---
+++ src/components/UI/SvgIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="24px" height="24px">
+    <use :xlink:href="id" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "SvgIcon",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
