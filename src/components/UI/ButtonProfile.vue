91059c1754646dff533d9a7cfca8b69c9af83045
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-23 14:16:10 +0300

    feat: Добавить ButtonProfile компонент

---
+++ src/components/UI/ButtonProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class=""><UserAvatar /><SvgIcon id="#dots" /></button>
+</template>
+
+<script>
+import SvgIcon from "./SvgIcon.vue";
+import UserAvatar from "./UserAvatar.vue";
+export default {
+  name: "ButtonProfile",
+  components: {
+    SvgIcon,
+    UserAvatar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+}
+</style>
