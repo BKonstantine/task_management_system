8f244c2a353bf97e8fdfdfd6be03acef2f081323
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-23 15:18:38 +0300

    feat: Добавить обработку svg спрайтов

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,19 @@ module.exports = defineConfig({
       },
     },
   },
+  pluginOptions: {
+    svgSprite: {
+      dir: "./src/assets/icons",
+      test: /\.svg$/,
+      loaderOptions: {
+        extract: false,
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("svg-sprite")
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader");
+  },
 });
