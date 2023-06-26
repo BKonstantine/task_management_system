c2e382af51161900930f8d7ef11bd25c576bed01
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-26 11:25:43 +0300

    feat: Добавить компонент ProfilePage

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <PageContainer><p>ProfilePage</p></PageContainer>
+</template>
+
+<script>
+import PageContainer from "@/components/UI/PageContainer.vue";
+export default {
+  name: "ProfilePage",
+  components: {
+    PageContainer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
