6a04fa402e86892df570210820bade7129d6d1e0
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-26 11:29:55 +0300

    feat: Добавить BaseText компонент

---
+++ src/components/UI/BaseText.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text"><slot /></p>
+</template>
+
+<script>
+export default {
+  name: "BaseText",
+};
+</script>
+
+<style lang="scss" scoped>
+.text {
+  @include font-setting(400, 14px, 19px);
+}
+</style>
