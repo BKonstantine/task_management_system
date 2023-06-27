79b588b2609f68b625000e34ff3e781cb70b5348
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-27 22:21:59 +0300

    feat: Добавить модули задач и проектов для стора

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import userModule from "./userModule";
+import taskModule from "./taskModule";
+import projectModule from "./projectModule";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     userModule,
+    taskModule,
+    projectModule,
   },
 });
