55b6ad9d48419e86587dbf139a201a169255033c
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-28 12:21:56 +0300

    feat: Сделать запрос проектов с сервера

--- src/store/projectModule.js
+++ src/store/projectModule.js
@@ -1,6 +1,40 @@
+import { getProjects } from "@/api/projects";
+
 export default {
-  state: {},
-  mutations: {},
+  state: {
+    projectsList: [],
+    projectsListRequest: false,
+    projectsListSuccess: false,
+    projectsListError: false,
+  },
+  mutations: {
+    setProjectsList(state, payload) {
+      state.projectsList = payload;
+    },
+    setProjectsListRequest(state, payload) {
+      state.projectsListRequest = payload;
+    },
+    setProjectsListSuccess(state, payload) {
+      state.projectsListSuccess = payload;
+    },
+    setProjectsListError(state, payload) {
+      state.projectsListError = payload;
+    },
+  },
   getters: {},
-  actions: {},
+  actions: {
+    fetchProjects({ commit }, projectData) {
+      commit("setProjectsListRequest", true);
+      getProjects(projectData)
+        .then((data) => {
+          commit("setProjectsList", data.projects);
+          commit("setProjectsListRequest", false);
+          commit("setProjectsListSuccess", true);
+        })
+        .catch(() => {
+          commit("setProjectsListSuccess", false);
+          commit("setProjectsListError", true);
+        });
+    },
+  },
 };
