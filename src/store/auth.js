dcd9ead8e08bac9619d9ece33548afc610a09412
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-04 10:12:09 +0300

    refactor: Внести изменения в authModule

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,6 +1,13 @@
 import { login } from "@/api/auth";
 import { saveToken } from "@/helpers/access-token";
 
+export const mutation = {
+  SET_AUTH: "SET_AUTH",
+  SET_AUTH_REQUEST: "SET_AUTH_REQUEST",
+  SET_AUTH_SUCCESS: "SET_AUTH_SUCCESS",
+  SET_AUTH_ERROR: "SET_AUTH_ERROR",
+};
+
 export default {
   state: {
     isAuth: false,
@@ -9,32 +16,32 @@ export default {
     isAuthError: false,
   },
   mutations: {
-    setAuth(state, payload) {
+    [mutation.SET_AUTH]: (state, payload) => {
       state.isAuth = payload;
     },
-    setAuthRequest(state, payload) {
+    [mutation.SET_AUTH_REQUEST]: (state, payload) => {
       state.isAuthRequest = payload;
     },
-    setAuthSuccess(state, payload) {
+    [mutation.SET_AUTH_SUCCESS]: (state, payload) => {
       state.isAuthSuccess = payload;
     },
-    setAuthError(state, payload) {
+    [mutation.SET_AUTH_ERROR]: (state, payload) => {
       state.isAuthError = payload;
     },
   },
   getters: {},
   actions: {
-    fetchLogin({ commit }) {
-      commit("setAuthRequest", true);
+    fetchLogin: ({ commit }) => {
+      commit(mutation.SET_AUTH_REQUEST, true);
       login()
         .then((data) => {
           saveToken(data.token);
-          commit("setAuth", true);
-          commit("setAuthRequest", false);
-          commit("setAuthSuccess", true);
+          commit(mutation.SET_AUTH, true);
+          commit(mutation.SET_AUTH_REQUEST, false);
+          commit(mutation.SET_AUTH_SUCCESS, true);
         })
         .catch(() => {
-          commit("setAuthError", false);
+          commit(mutation.SET_AUTH_ERROR, false);
         });
     },
   },
