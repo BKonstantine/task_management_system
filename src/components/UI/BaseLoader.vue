aaccdc77e88b23ff6ed9f093a54442f1fc05cf8a
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-04 09:59:17 +0300

    feat: Добавить компонент BaseLoader

---
+++ src/components/UI/BaseLoader.vue
@@ -0,0 +1,14 @@
+<template>
+  <SvgIcon id="#loader" class="loader" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  width: 40px;
+  height: 40px;
+}
+</style>
