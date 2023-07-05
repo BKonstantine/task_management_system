70dee7570d74158c5e72f8ef9998d91d8d6be693
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-05 11:41:11 +0300

    refactor: Изменить стили BaseInput и BaseTextarea

--- src/components/UI/BaseInput.vue
+++ src/components/UI/BaseInput.vue
@@ -24,6 +24,15 @@ export default {
   @include font-setting(400, 14px, 19px);
   cursor: text;
 
+  &:hover {
+    border: 1px solid $border-color-hover;
+  }
+
+  &:focus {
+    border: 1px solid $border-color-active;
+    outline: none;
+  }
+
   &::placeholder {
     font-family: "Open Sans", sans-serif;
     color: $font-color-placeholder;
